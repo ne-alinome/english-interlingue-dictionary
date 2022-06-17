257c7ff64e64affc17f873e5abd2824c3336bba8
ne_alinome <github@programandala.net>
2022-06-17 17:12:46 +0000

    Rewrite/fix/improve the data to YAML converter.
    FossilOrigin-Name: e746e7c43114f210d6154a7ca5102a99a007384629f2353a6b137c749cd655bc

--- make/convert_data_to_yaml.fish
+++ make/convert_data_to_yaml.fish
@@ -10,55 +10,77 @@
 
 # This fish program converts the source data to the YAML format.
 
-# Last modified 20220617T0038+0200.
+# Last modified 20220617T1655+0200.
 # See change log at the end of the file.
 
 # ==============================================================
 
 set output $argv[]
 
-set line_number 1
 set previous_headword ''
+set previous_type ''
+set indent_level 0
+set listing_examples false
+
+function indent
+	echo (string repeat --no-newline --count $indent_level '  ')
+end
 
 while read line
-
-	# if string match --quiet '*##*' "$line"
-	# 	echo "WARNING: blank field in line $line_number:"
-	# 	echo $line
-	# end
 
 	set headword   (string split --field 1 --allow-empty '#' "$line")
 	set type       (string split --field 2 --allow-empty '#' "$line")
 	set definition (string split --field 3 --allow-empty '#' "$line")
 
-	# XXX FIXME This fails when the string starts with a hyphen,
-	# e.g. the definition of a suffix, because fish interprets it
-	# as an option of `string trim`; the solution is to add a leading
-	# space:
+	# XXX FIXME `string trim` fails with error "unknown option -- in command
+	# substitution" when the trimmed string starts with a hyphen,
+	# e.g. the definition of a suffix; the workaround is to add a leading
+	# space to it:
 
 	set headword   (string trim " $headword")
 	set type       (string trim " $type")
 	set definition (string trim " $definition")
 
 	if string match --quiet '*-example' "$type"
-		# The type of the entry is "example", used just to force
-		# the proper sorting, not to be displayed.
-		set type 'example'
-	end
 
-	if not test "$headword" = "$previous_headword"
-		# The record is a new headword.
-		set previous_headword "$headword"
-		echo "$headword:"
-	end
+		# The type of the entry is "example".
 
-	if test -n "$definition"
-		echo "  - $type: $definition"
+		if not $listing_examples
+			# Start a list of examples.
+			set listing_examples true
+			set indent_level (math $indent_level+1)
+			echo (indent)"examples:"
+			set indent_level (math $indent_level+1)
+		end
+
+		set example     (string trim (string split --field 1 ':' $definition))
+		set translation (string trim (string split --field 2 ':' $definition))
+		echo (indent)"- example:     $example"
+		echo (indent)"  translation: $translation"
+	
 	else
-		echo "  - $type"
-	end
 
-	set line_number (math $line_number+1)
+		# The type of the entry is not "example".
+	
+		if $listing_examples
+			# Deactivate the current listing of examples.
+			set listing_examples false
+			set indent_level (math $indent_level-2)
+		end
+
+		if not test "$headword" = "$previous_headword"
+			# The entry is a new headword.
+			set previous_headword "$headword"
+			echo '---'
+			echo (indent)"entry: $headword"
+		end
+
+		echo (indent)"- function: $type"
+		if test -n "$definition"
+			echo (indent)"  translation: $definition"
+		end
+
+	end
 
 end
 
